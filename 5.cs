d372ed25da163235340cea4d1629e089e539b965
prokopenko-maxim <75616276+prokopenko-maxim@users.noreply.github.com>
2021-06-11 09:38:09 +0300

    Add files via upload

---
+++ 5.cs
@@ -0,0 +1,21 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		// Try with m = 100 and then with m = 15.
+		int m = 98;
+		int n = 10;
+
+		if (m < 100)
+   			if (n < 15)
+   	    {
+        	Console.WriteLine("Оба зала свободны");
+   	 	}
+    	else
+    	{
+        	Console.WriteLine("Один зал свободен");
+    	}
+	}
+}
